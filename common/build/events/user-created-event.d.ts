a434da971bd22f165bb749fd2a9888181576e301
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-03-01 12:24:48 +0200

    Updates

--- common/build/events/user-created-event.d.ts
+++ common/build/events/user-created-event.d.ts
@@ -9,7 +9,6 @@ export interface UserCreatedEvent {
         picture: string;
         specialization: string;
         phone: string;
-        availableDate: string;
         version: number;
     };
 }
