bc45f7a9df2242027979f57c0d17b46d2eff7075
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-03-02 09:26:27 +0200

    Update user.model.ts

--- appointment/src/models/user.model.ts
+++ appointment/src/models/user.model.ts
@@ -4,12 +4,14 @@ import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface UserAttrs {
     id: string;
+    username: string;
     email: string;
-    picture: string;
-    specialization?: string;
-    phone?: string;
     role: RoleType;
-    availableDates: string[];
+    picture: string;
+    age: number;
+    specialization?: string;
+    phone: string;
+    version: number;
 };
 
 interface UserDoc extends mongoose.Document {
@@ -19,7 +21,6 @@ interface UserDoc extends mongoose.Document {
     phone?: string;
     role: RoleType;
     availableDates: string[];
-    rate: number;
     version: number;
     createdAt: string;
     updatedAt: string;
@@ -27,6 +28,7 @@ interface UserDoc extends mongoose.Document {
 
 interface UserModel extends mongoose.Model<UserDoc> {
     build(attrs: UserAttrs): UserDoc;
+    findByEvent(event: { id: string; version: number; }): Promise<UserDoc | null>;
 }
 
 const userSchema = new mongoose.Schema({
@@ -54,12 +56,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         trim: true,
     },
-    rate: {
-        type: Number,
-        default: 0,
-        max: 5,
-        min: 0
-    },
     availableDates: {
         type: Array,
         default: []
@@ -72,11 +68,6 @@ const userSchema = new mongoose.Schema({
                 delete ret.password;
             if (ret.availableDates.length === 0) {
                 delete ret.availableDates;
-            } else {
-                ret.availableDates = ret.availableDates.map((data: any) => {
-                    const { date, start_time, end_time, _id: id } = data;
-                    return { date, start_time, end_time, id };
-                });
             }
         }
     },
@@ -88,6 +79,13 @@ userSchema.set("versionKey", "version");
 userSchema.plugin(updateIfCurrentPlugin);
 
 userSchema.statics.build = (attrs: UserAttrs) => new User({ _id: attrs.id, ...attrs });
+
+userSchema.statics.findByEvent = (event: { id: string; version: number; }) => {
+    return User.findOne({
+        id: event.id,
+        version: event.version - 1
+    });
+};
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
