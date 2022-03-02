536a10923ea15295f416946c5a985642d2dd4119
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-03-02 09:26:15 +0200

    Create user-updated-listener.ts

---
+++ appointment/src/events/listeners/user-updated-listener.ts
@@ -0,0 +1,27 @@
+import { Listener, Subjects, UserUpdatedEvent } from "@clinic-services/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { User } from "../../models/user.model";
+
+export class UserUpdatedListener extends Listener<UserUpdatedEvent> {
+    readonly subject = Subjects.UserUpdated;
+    queueGroupName = queueGroupName;
+    async onMessage(data: UserUpdatedEvent["data"], msg: Message) {
+
+        const user = await User.findByEvent(data);
+
+        if (!user) {
+            throw new Error("User Not Found");
+        }
+
+        let fields: { [key: string]: any; } = { ...data };
+
+        delete fields["version"];
+
+        user.set({ ...fields });
+
+        await user.save();
+
+        msg.ack();
+    }
+}
